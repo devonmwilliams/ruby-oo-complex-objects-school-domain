ffeff01b9a4ba62dbc07e1e836c65b3d4bee6e5f
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-28 00:52:04 -0500

    commit tree 98f234aa97896e6383e052bc49f5da0b5935200f

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+
+
+
+class School
+
+    attr_reader :roster, :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+  
+    def add_student(student_name, grade)
+        if roster.has_key?(grade)
+            roster[grade] << name
+        else roster[grade] = [name]
+        end
+    end
+
+    def grade(student_grade)
+        roster[student_grade]
+    end
+
+    def sort
+        roster.each do |grade, students|
+            sorted[grade] = students.sort
+        end
+    end
+end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'relative'
 
 describe 'School' do
   before :each do
